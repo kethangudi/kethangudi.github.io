55c685d1ffcc35eb174fe58a904ef7b8f54ae490
kethangudi <103330664+kethangudi@users.noreply.github.com>
2026-02-08 17:00:52 +0000

    Deploying to gh-pages from @ kethangudi/kethangudi.github.io@ff20fa87f2590d47f42914d1fcc21248fea66833 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,12 +45,12 @@ ninja.data = [{
               window.location.href = "/books/the_godfather/";
             },},{id: "projects-custom-discrete-25w-buck-converter",
           title: 'Custom Discrete 25W Buck Converter',
-          description: "Current-Controlled",
+          description: "Current-Controlled LED Driver",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-autonomous-cruise-control-page-under-construction",
+          title: 'Autonomous Cruise Control [PAGE UNDER CONSTRUCTION]',
+          description: "Car PID Controller + Sensing and CAN Firmware",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
